132266facdc8d19622ccd14d597b6f2a881c1dd1
yixu <903568702@qq.com>
2025-02-24 18:27:25 +0800

    add metrics prometheus in cart-service

--- app/cart/infra/rpc/client.go
+++ app/cart/infra/rpc/client.go
@@ -5,16 +5,19 @@ import (
 	"Gomall/app/cart/utils"
 	"Gomall/rpc_gen/kitex_gen/cart/cartservice"
 	"Gomall/rpc_gen/kitex_gen/product/productcatalogservice"
+	"Gomall/common/clientsuite"
 	"sync"
 
 	"github.com/cloudwego/kitex/client"
-	consul "github.com/kitex-contrib/registry-consul"
 )
 
 var (
 	CartClient cartservice.Client
 	ProductClient productcatalogservice.Client
 	once sync.Once
+	ServiceName = conf.GetConf().Kitex.Service
+	RegistryAddr = conf.GetConf().Registry.RegistryAddress[0]
+	err	error
 ) 
 
 func InitClient() {
@@ -24,8 +27,13 @@ func InitClient() {
 }
 
 func initProductClient() {
-	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])//"127.0.0.1:8500"
-	utils.MustHandleError(err)
-	CartClient, err = cartservice.NewClient("product", client.WithResolver(r))
+	 opts := []client.Option{
+		client.WithSuite(clientsuite.CommonClientSuite{
+			CurrentServiceName: ServiceName,
+			RegistryAddr:       RegistryAddr,
+		}),
+	 }
+
+	ProductClient, err = productcatalogservice.NewClient("product", opts...)
 	utils.MustHandleError(err)
 }
--- app/cart/main.go
+++ app/cart/main.go
@@ -7,18 +7,25 @@ import (
 	"Gomall/app/cart/biz/dal"
 	"Gomall/app/cart/conf"
 	"Gomall/app/cart/infra/rpc"
+	"Gomall/common/mtl"
+	"Gomall/common/serversuite"
 	"Gomall/rpc_gen/kitex_gen/cart/cartservice"
 
 	"github.com/cloudwego/kitex/pkg/klog"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	_ "github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
-	consul "github.com/kitex-contrib/registry-consul"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+var (
+	ServiceName = conf.GetConf().Kitex.Service
+	RegistryAddr = conf.GetConf().Registry.RegistryAddress[0]
+)
+
 func main() {
+	mtl.InitMetric(ServiceName, conf.GetConf().Kitex.MetricsPort, RegistryAddr)
 	dal.Init()
 	rpc.InitClient()
 	opts := kitexInit()
@@ -37,18 +44,11 @@ func kitexInit() (opts []server.Option) {
 	if err != nil {
 		panic(err)
 	}
-	opts = append(opts, server.WithServiceAddr(addr))
-
-	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
-    if err != nil {
-        klog.Fatal(err)
-    }
-	opts = append(opts, server.WithRegistry(r))
-
-	// service info
-	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-		ServiceName: conf.GetConf().Kitex.Service,
+	opts = append(opts, server.WithServiceAddr(addr), server.WithSuite(serversuite.CommonServerSuite{
+		CurrentServiceName: ServiceName,
+		RegistryAddr:		RegistryAddr,
 	}))
+
 
 	// klog
 	logger := kitexlogrus.NewLogger()
